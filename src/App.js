e59ec9f22af6d74cd3a120d8b059f0c9a43e13f5
asosiymenu007 <asosiymenu007@gmail.com>
2024-04-17 17:00:46 +0500

    vazifa bajarildi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [rasim, setRasim] = useState(0);
+
+  const images = [
+    "/assets/night-sky.jpg",
+    "/assets/2.jpg",
+    "/assets/3.jpg",
+    "/assets/4.jpg",
+  ];
+
+  const handleNextClick = () => {
+    if (rasim < images.length - 1) {
+      setRasim(rasim + 1);
+    }
+  };
+
+  const handleBackClick = () => {
+    if (rasim > 0) {
+      setRasim(rasim - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={images[rasim]} alt="" width={300} /> <br />
+      <br />
+      <button onClick={handleBackClick}>Back</button>
+      <button onClick={handleNextClick}>Next</button>
     </div>
   );
 }
 
 export default App;
+
+
